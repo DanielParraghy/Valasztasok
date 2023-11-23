264265200e1bb1215e1b1ca1c0ec3e29cb1f1b54
DanielParraghy <parraghydaniel@gmail.com>
2023-11-23 12:29:40 +0100

    megoldas osztaly bovitve

--- src/Megoldas.ts
+++ src/Megoldas.ts
@@ -1,5 +1,10 @@
+import { ServerResponse } from "http";
 import Politikus from "./Politikus";
 
 export default class Megoldas {
     #ve: Politikus[] = [];
+
+    constructor(állományNeve: string) {
+        // beolvasás + példányosítás + tárolás
+    }
 }
